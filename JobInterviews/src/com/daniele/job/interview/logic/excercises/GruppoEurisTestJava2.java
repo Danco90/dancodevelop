f8bac7c564880514a905958fd307fb2875d64b91
alecarr74 <mariaalessandracarriero74@gmail.com>
2017-01-06 22:37:24 +0100

    created new project JobInterviews

---
+++ JobInterviews/src/com/daniele/job/interview/logic/excercises/GruppoEurisTestJava2.java
@@ -0,0 +1,41 @@
+package com.daniele.job.interview.logic.excercises;
+
+
+/**
+ * 
+ * @author daniele
+ * @version 0.1
+ * @updated 06/01/2017
+ *
+ */
+public class GruppoEurisTestJava2 {
+	
+	
+	public void parseFile(String fileInPath, String fileOutPath){
+		
+	}
+	
+	private void readInvoices(String fileInPath, String fileOutPath){
+		
+	}
+	
+	
+	private void writeInvoicesOrderedByPayMethod(){
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GruppoEurisTestJava2 test2 = new GruppoEurisTestJava2();
+		
+		String fileInPath = "";
+		String fileOutPath = "";
+		
+		test2.parseFile(fileInPath, fileOutPath);
+		
+
+	}
+
+}
